a4c2152f80104505a475ce47e1610b2396108cd6
JamalGuyo <jamalguyo08@gmail.com>
2021-01-20 10:37:40 +0300

    demo pipes, ngStyle and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,61 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullName: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      <h1>Using *ngIf directive</h1>
-      <input type="text" [(ngModel)]="title" />
-      <ng-template [ngIf]="title.length">
-        <div>
-          <h2>searching for : {{ title }}</h2>
-        </div>
-      </ng-template>
-      <div *ngIf="title.length">
-        <h2>searching for : {{ title }}</h2>
-      </div>
+      <h1>List of passengers</h1>
+      <ul>
+        <li *ngFor="let p of passengers; let i = index">
+          <span
+            class="status"
+            [ngStyle]="{
+              backgroundColor: p.checkedIn ? 'green' : 'red'
+            }"
+          ></span>
+          <!-- <span
+            class="status"
+            [style.backgroundColor]="p.checkedIn ? 'green' : 'red'"
+          ></span> -->
+          <!-- <span class="status" [ngClass]="{ 'checked-in': p.checkedIn }"></span> -->
+          <!-- <span class="status" [class.checked-in]="p.checkedIn"></span> -->
+          {{ i }} {{ p.fullName }}
+          <p>
+            {{ p | json }}
+          </p>
+        </li>
+      </ul>
     </div>
   `,
 })
 export class AppComponent {
-  title: String = 'ultimate course';
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullName: 'John',
+      checkedIn: true,
+    },
+    {
+      id: 2,
+      fullName: 'Jane',
+      checkedIn: true,
+    },
+    {
+      id: 1,
+      fullName: 'Jacob',
+      checkedIn: false,
+    },
+    {
+      id: 2,
+      fullName: 'Dun',
+      checkedIn: false,
+    },
+  ];
 }
